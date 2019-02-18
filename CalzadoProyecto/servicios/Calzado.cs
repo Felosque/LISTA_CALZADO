67b28745566c3778cc46c73e5d4b4bf221570359
Felosque <felipelondono4@gmail.com>
2019-02-17 23:11:51 -0500

    Nodos añadidos y algunos comandos

---
+++ CalzadoProyecto/servicios/Calzado.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalzadoProyecto.servicios
+{
+    class Calzado
+    {
+
+        private String tipo;
+
+        private int talla;
+
+        private double precio;
+
+        private DateTime fechaCompra;
+
+        private Calzado sgte;
+
+        public Calzado(String pTipo, int pTalla, double pPrecio, DateTime pFecha)
+        {
+            tipo = pTipo;
+            talla = pTalla;
+            precio = pPrecio;
+            fechaCompra = pFecha;
+            sgte = null;
+        }
+
+        public String darTipo()
+        {
+            return tipo;
+        }
+
+        public void cambiarTipo(String pTipo)
+        {
+            tipo = pTipo;
+        }
+
+        public int darTalla()
+        {
+            return talla;
+        }
+
+        public void cambiarTalla(int pTalla)
+        {
+            talla = pTalla;
+        }
+
+        public double darPrecio()
+        {
+            return precio;
+        }
+
+        public void cambiarPrecio(double pPrecio)
+        {
+            precio = pPrecio;
+        }
+
+        public DateTime darFechaDeCompra()
+        {
+            return fechaCompra;
+        }
+
+        public void cambiarFechaDeCompra(DateTime pFechaCompra)
+        {
+            fechaCompra = pFechaCompra;
+        }
+
+        public Calzado darSiguiente()
+        {
+            return sgte;
+        }
+
+        public void cambiarSiguiente(Calzado pSgte)
+        {
+            sgte = pSgte;
+        }
+
+    }
+}
--- CalzadoProyecto/servicios/Servicios.cs
+++ CalzadoProyecto/servicios/Servicios.cs
@@ -8,5 +8,56 @@ namespace CalzadoProyecto.servicios
 {
     class Servicios
     {
+        private Calzado cabecera;
+
+
+        public void adicionarCalzadoAlInicio(Calzado pCalzado)
+        {
+            if(cabecera == null){
+                cabecera = pCalzado;
+            }
+            else{
+                pCalzado.cambiarSiguiente(cabecera);
+                cabecera = pCalzado;
+            }
+        }
+
+        public void adicionarCalzadoAlFinal(Calzado pCalzado)
+        {
+            if (cabecera == null){
+                cabecera = pCalzado;
+            }
+            else{
+                darUltimoCalzado().cambiarSiguiente(pCalzado);
+            }
+        }
+
+        public void adicionarCalzadoEnMedio(Calzado pCalzado, int pNumero)
+        {
+            Calzado recorrido = cabecera;
+            Boolean salir = false;
+            for(int i = 1; i == pNumero - 1 && !salir && cabecera != null; i++)
+            {
+                recorrido = recorrido.darSiguiente();
+                if(recorrido == null)
+                {
+                    salir = true;
+                }
+            }
+
+            Calzado tmp = recorrido.darSiguiente();
+            recorrido.cambiarSiguiente(pCalzado);
+            pCalzado.cambiarSiguiente(tmp);
+        }
+
+        public Calzado darUltimoCalzado()
+        {
+            Calzado recorrido = cabecera;
+            while(recorrido.darSiguiente() != null){
+                recorrido = recorrido.darSiguiente();
+            }
+            return recorrido;
+        }
+
     }
 }
